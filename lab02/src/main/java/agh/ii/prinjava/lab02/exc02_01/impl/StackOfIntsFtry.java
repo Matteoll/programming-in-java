eca596d561e1496d02ad21f8598b19b2b40103f7
Matteo <matteo.lambert@efrei.net>
2023-10-06 04:49:05 +0200

    final lab02 commit

--- lab02/src/main/java/agh/ii/prinjava/lab02/exc02_01/impl/StackOfIntsFtry.java
+++ lab02/src/main/java/agh/ii/prinjava/lab02/exc02_01/impl/StackOfIntsFtry.java
@@ -3,24 +3,33 @@ package agh.ii.prinjava.lab02.exc02_01.impl;
 import agh.ii.prinjava.lab02.exc02_01.StackOfInts;
 
 /**
- * Stack of integers factory
+ * Stack of integers factory that provides different implementations.
  */
 public final class StackOfIntsFtry {
     private StackOfIntsFtry() {
     }
 
+    /**
+     * Enum representing available stack implementations.
+     */
     enum Impln {
         /**
-         * Array Based
+         * Array Based Stack Implementation
          */
         ARRAY_B,
 
         /**
-         * (Custom) Linked List Based
+         * Custom Linked List Based Stack Implementation
          */
         L_LIST_B //
     }
 
+    /**
+     * Creates a new instance of a stack of integers based on the specified implementation.
+     *
+     * @param s The implementation type to create
+     * @return A stack of integers with the specified implementation
+     */
     static StackOfInts create(Impln s) {
         return switch (s) {
             case ARRAY_B -> new ArrayBasedImpl();
@@ -28,3 +37,11 @@ public final class StackOfIntsFtry {
         };
     }
 }
+
+
+
+
+
+
+
+
